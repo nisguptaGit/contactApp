a405ac7f40ea647a3b012cc2f912aef429d5a3e7
Nishant Gupta <nishant442201@gmail.com>
2019-04-12 10:30:43 +0530

    Update index.js

--- contact-app/src/sagas/index.js
+++ contact-app/src/sagas/index.js
@@ -1,5 +1,4 @@
-import { delay } from 'redux-saga';
-import { take, call, put, select, takeLatest, takeEvery } from 'redux-saga/effects';
+import {  call, put, takeLatest/*,  select, take, takeEvery*/ } from 'redux-saga/effects';
 import * as actions from '../actions';
 import * as constants from '../constants';
 import * as apis from '../apis';
@@ -20,10 +19,25 @@ export function* callFetchContactsList() {
   }
 }
 
+export function* doDeleteContact(action) {
+//Do delete operation in saga
+  if(action.data){
+    if(action.data.value === constants.DELETE_ALL_CONTACTS_TEXT){
+      yield put(actions.deleteAllContacts())
+    } else {
+      yield put(actions.deleteContact(action.data.name))
+    }
+  }
+}
+
 export function* watchFetchContactsListRequest() {
   yield takeLatest(constants.GET_ALL_CONTACTS, callFetchContactsList);
 }
+export function* watchDeleteContactRequest() {
+  yield takeLatest(constants.DELETE_CONTACT_OPERATION, doDeleteContact);
+}
 
 export default [
-  watchFetchContactsListRequest
+  watchFetchContactsListRequest,
+  watchDeleteContactRequest
 ];
